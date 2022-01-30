afb4b6fc127f141034250558458bf5649fba24fb
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-01-30 14:39:33 -0500

    bccrypt

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,6 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-
-const sequelize = require('../config/connection');
+const sequelize = require('../Develop/config/connection');
 
 class ProductTag extends Model {}
 
